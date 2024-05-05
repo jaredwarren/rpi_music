45f94012297b281e427dd751a85f9873f703a05d
Jared Warren <jlwarren1@gmail.com>
2024-05-05 13:49:26 -0600

    debug

--- downloader/youtube-dl.go
+++ downloader/youtube-dl.go
@@ -33,6 +33,7 @@ func (d *YoutubeDLDownloader) DownloadVideo(videoID string, logger log.Logger) (
 
 	// https://music.youtube.com/watch?v=4qwIhKfv_Dc&si=ST0cFoZIDwj5DKDI
 	videoID = strings.Replace(videoID, "//music.", "//", 1)
+	fmt.Printf("~~~~~~~~~~~~~~~\n %+v\n\n", videoID)
 
 	g := new(errgroup.Group)
 
@@ -42,6 +43,7 @@ func (d *YoutubeDLDownloader) DownloadVideo(videoID string, logger log.Logger) (
 		if err == nil {
 			resp.Title = info["title"].(string)
 		}
+		fmt.Printf("~~~~~~~~~~~~~~~\n getVideoInfo err::\n%+v\n\n", err)
 		return err
 	})
 
@@ -49,12 +51,15 @@ func (d *YoutubeDLDownloader) DownloadVideo(videoID string, logger log.Logger) (
 	g.Go(func() error {
 		var err error
 		filename, err = GetVideoFilename(videoID)
+		fmt.Printf("~~~~~~~~~~~~~~~\n GetVideoFilename err:\n%+v\n\n", err)
 		return err
 	})
 
 	// download video
 	g.Go(func() error {
-		return downloadVideo(videoID)
+		err := downloadVideo(videoID)
+		fmt.Printf("~~~~~~~~~~~~~~~\n downloadVideo err::\n%+v\n\n", err)
+		return err
 	})
 
 	if err := g.Wait(); err != nil {
@@ -88,10 +93,16 @@ func getVideoInfo(videoID string) (map[string]interface{}, error) {
 	cmd := exec.Command("yt-dlp", args...)
 	std, err := cmd.Output()
 	if err != nil {
-		return nil, err
+		fmt.Printf("~~~~~~~~~~~~~~~\n getvideoInfo err:\n%+v\n\n", err)
+		fmt.Printf("~~~~~~~~~~~~~~~\n getvideoInfo out:\n%+v\n\n", std)
+		return nil, fmt.Errorf("cmd err:%w", err)
 	}
 	out := map[string]interface{}{}
-	json.Unmarshal(std, &out)
+	err = json.Unmarshal(std, &out)
+	if err != nil {
+		fmt.Printf("~~~~~~~~~~~~~~~\n getvideoInfo out:\n%+v\n\n", std)
+		return nil, fmt.Errorf("json err:%w", err)
+	}
 	return out, nil
 }
 
@@ -127,7 +138,12 @@ func downloadVideo(videoID string) error {
 	}
 	args = append(args, videoID)
 	cmd := exec.Command("yt-dlp", args...)
-	_, err := cmd.Output()
+	std, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("~~~~~~~~~~~~~~~\n downloadVideo err:\n%+v\n\n", err)
+		fmt.Printf("~~~~~~~~~~~~~~~\n downloadVideo out:\n%+v\n\n", std)
+		return err
+	}
 
 	//youtube-dl --ignore-errors --no-call-home --no-cache-dir --restrict-filenames -f bestaudio -o "song_files/%(title)s-%(id)s.%(ext)s" "https://youtu.be/7s1UKDdB0OU?si=jpi0XTovMtQ1F44Z"
 	// yt-dlp -o "song_files/%(title)s-%(id)s.%(ext)s" "https://youtu.be/7s1UKDdB0OU?si=jpi0XTovMtQ1F44Z"
@@ -166,6 +182,11 @@ func downloadVideoThumb(videoID string) (string, error) {
 	args = append(args, videoID)
 	cmd := exec.Command("yt-dlp", args...)
 	std, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("~~~~~~~~~~~~~~~\n downloadVideoThumb err:\n%+v\n\n", err)
+		fmt.Printf("~~~~~~~~~~~~~~~\n downloadVideoThumb out:\n%+v\n\n", std)
+		return "", err
+	}
 
 	// parse output because I can't find a better way to get thumb name
 	outStr := string(std)
